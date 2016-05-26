866879a3beeea132fcb4e48947c821a7ab11691e
Carlos Arguelles <caad@mit.edu>
2016-05-26 02:19:45 -0500

    f11

--- nusquids.rb
+++ nusquids.rb
@@ -1,9 +1,10 @@
 # squids instalation formulae
 # C. Arguelles (2016)
 
-class NuSquids < Formula
+class Nusquids < Formula
   homepage "https://github.com/arguelles/nuSQuIDS"
   url "https://github.com/arguelles/nuSQuIDS/archive/0.1.tar.gz"
+  sha256 "288f19896918f4a744e4e74fdbe73ad2dda70781ed19db2d230a7827f2159a67"
 
   depends_on "gsl"
   depends_on "squids"
