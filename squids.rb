af048e3afb0e301e85ff50411209eb5d14c544ea
Carlos Arguelles <caad@mit.edu>
2016-05-26 02:13:14 -0500

    f8

--- squids.rb
+++ squids.rb
@@ -4,6 +4,7 @@
 class Squids < Formula
   homepage "https://github.com/jsalvado/SQuIDS"
   url "https://github.com/jsalvado/SQuIDS/archive/v1.1.tar.gz"
+  md5 "bfbe306039ccc52bb81586a1c48274eb"
 
   depends_on "gsl"
 
