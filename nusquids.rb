9943dc1330f0ac7b7b45168767e267aacaaa380b
Carlos Arguelles <caad@mit.edu>
2016-05-26 01:36:07 -0500

    add formulaes

---
+++ nusquids.rb
@@ -0,0 +1,20 @@
+# squids instalation formulae
+# C. Arguelles (2016)
+
+class nusquids < Formula
+  homepage "https://github.com/arguelles/nuSQuIDS"
+  url "https://github.com/arguelles/nuSQuIDS/archive/0.1.tar.gz"
+
+  depends_on "gsl"
+  depends_on "squids"
+  depends_on "hdf5"
+
+  def install
+    system "./configure"
+    system "make","install"
+  end
+
+  test do
+    system "true"
+  end
+end
---
+++ squids.rb
@@ -0,0 +1,18 @@
+# squids instalation formulae
+# C. Arguelles (2016)
+
+class squids < Formula
+  homepage "https://github.com/jsalvado/SQuIDS"
+  url "https://github.com/jsalvado/SQuIDS/archive/v1.1.tar.gz"
+
+  depends_on "gsl"
+
+  def install
+    system "./configure"
+    system "make","install"
+  end
+
+  test do
+    system "true"
+  end
+end
